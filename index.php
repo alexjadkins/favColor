e6d3ad11762398ec7be5f6b3bb71da90e495f698
alexjadkins <38173788+alexjadkins@users.noreply.github.com>
2018-04-08 01:16:41 -0400

    Update index.php

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
     			for($j = 0;$j<6;$j++){
         			echo "<img src='".$pics[$j]."'/>";
 				
-    				$c = file_get_contents($pics[$j]);
+    				$c = file_put_contents($pics[$j]);
     				$image = imagecreatefromstring($c);// imagecreatefromjpeg/png/
     				$width = imagesx($image);
 				$height = imagesy($image);
